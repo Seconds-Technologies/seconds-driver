216fa3c36493b63b9ee08b2e37d851fff3b681c1
Chipzstar <chipzstar.dev@googlemail.com>
2022-02-21 17:57:23 +0000

    [EAS Build] Run EAS Build for Android

--- app.config.js
+++ app.config.js
@@ -3,11 +3,8 @@ import 'dotenv/config';
 export default {
 	name: "seconds-driver",
 	version: '1.0.0',
-	android: {
-		package: 'com.secondstechnologies.secondsdriver'
-	},
 	extra: {
-		apiURL: process.env.REACT_APP_API_BASE_URL,
-		serverURL: process.env.REACT_APP_SERVER_BASE_URL,
+		apiURL: process.env.API_BASE_URL,
+		serverURL: process.env.SERVER_BASE_URL,
 	},
 };
