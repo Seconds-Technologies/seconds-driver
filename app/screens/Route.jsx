63e2211150ee5d3e1334989ea480f06b1d3bcd08
Chipzstar <chipzstar.dev@googlemail.com>
2022-03-25 19:23:27 +0000

    status UI fix for route orders

--- app/screens/Route.jsx
+++ app/screens/Route.jsx
@@ -5,8 +5,8 @@ import { useTailwind } from "tailwind-rn";
 import { useSelector } from "react-redux";
 
 const Route = ({ route }) => {
-	const tailwind = useTailwind()
-	const jobs = useSelector(state => state['jobs'].routeJobs.filter(({ routeOptimization: {routeId} }) => routeId === route.key))
+	const tailwind = useTailwind();
+	const jobs = useSelector(state => state["jobs"].routeJobs.filter(({ routeOptimization: { routeId } }) => routeId === route.key));
 	return (
 		<View style={tailwind("bg-white md:mx-32 py-5 px-5 border-0 md:border-4 border-gray-300 md:rounded-xl min-h-full")}>
 			<View style={tailwind("flex grow justify-center mt-5")}>
@@ -14,7 +14,12 @@ const Route = ({ route }) => {
 					keyExtractor={item => item._id.toString()}
 					data={jobs}
 					renderItem={({ item, index }) => (
-						<OrderCard key={index} item={item} bgStyle={"bg-completed-50"} textStyle={"text-completed-600"} />
+						<OrderCard
+							key={index}
+							item={item}
+							bgStyle={`bg-${item.status.toLowerCase()}-100`}
+							textStyle={`text-${item.status.toLowerCase()}-600`}
+						/>
 					)}
 				/>
 			</View>
